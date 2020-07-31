8418c5a36a32b0fb719289eb0e1be624bf158d96
Ran Marciano <ranm13@gmail.com>
2020-07-31 15:21:10 +0300

    created the basic blueprint for the web component

---
+++ tooltip.js
@@ -0,0 +1,7 @@
+class Tooltip extends HTMLElement{
+    constructor(){
+        super();
+    }
+}
+
+customElements.define("rm-tooltip", Tooltip); //by this you define your own html tag - built in js (the name must have dash - )
